bd1f1b1af438b82d5403f3a9fe12d063ea137fb7
Damien Tailhades <damien.tailhades@gmail.com>
2019-11-22 11:03:52 +0100

    First commit - react-modele

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+import './app.scss';
+
+// == Composant
+const App = () => (
+  <div id="app">
+    Wishes me App
+  </div>
+);
+
+// == Export
+export default App;
